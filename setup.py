f6bdd7aa3564c307fedb3ca61469d8a27c3b0400
Sinesipo <48671920+Sinesipo@users.noreply.github.com>
2019-03-19 14:54:45 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='package_root',
+    version='0.2',
+    packages=find_packages(exclude=['tests*']),
+    license='MIT',
+    description='EDSA example python package',
+    long_description=open('README.md').read(),
+    install_requires=['numpy'],
+    url='https://github.com/Sinesipo/package_root.git',
+    author='Sinesipo',
+    author_email='smbasa1818@gmail.com'
+)
